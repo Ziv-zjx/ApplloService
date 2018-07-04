a93eb0f87720bb0dfc7e6e7cd0db6884810fa265
Administrator <Administrator@IQ2M3AU68J8CM5E>
2018-07-04 14:30:47 +0800

    添加项目文件。

---
+++ FirstApplloService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstApplloService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
